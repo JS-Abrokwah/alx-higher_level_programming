a5d1fcc98166ccd9d6932a1b57a90a508a8432d3
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-03-10 22:46:18 +0000

    Hidden name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     count = int(len(sys.argv)) - 1
     results = 0
     for i in range(count):
-        results += int(sys.argv[i+1])
+        results += int(sys.argv[i + 1])
     print("{}".format(results))
