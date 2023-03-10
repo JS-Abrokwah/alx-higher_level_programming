18c549552b1d059a289f8edbb9480ae81e043c12
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-03-10 22:23:57 +0000

    infinte addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    count = int(len(sys.argv)) - 1
+    results = 0
+    for i in range(count):
+        results += int(sys.argv[i+1])
+    print("{}".format(results))
