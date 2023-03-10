95fdf0184b834feed3fb589bce5e539416d7d1d2
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-03-10 23:21:46 +0000

    Dynamic Calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    op = sys.argv[2]
+
+    match op:
+        case "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        case "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        case "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        case "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
