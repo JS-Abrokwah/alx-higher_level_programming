a06b3e6ceedc0d95901431c1e64512c1ca17aa63
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-03-15 23:43:00 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new = [my_list[:1]]
-    for i in my_list:
-        if i is not in new:
-            new.append(i)
-    add = 0
-    for i in new:
-        add += i
-    return add
+    addition = 0
+    for i in set(my_list):
+        addition += i
+    return addition
