e3836bbd08f9ce8252cb23e1f21869b42fc51d5d
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-04-11 22:15:06 +0000

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    Defines a function the determine if an
+    object is an instance of a class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if obj is an instance of a_class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
