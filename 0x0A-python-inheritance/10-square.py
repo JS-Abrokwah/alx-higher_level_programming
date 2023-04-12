a69dde9fe9f5802bdc92a98163027b1d7a032374
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-04-12 00:07:30 +0000

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+""" Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines an empty class"""
+    def area(self):
+        """area() is not implemented. yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
