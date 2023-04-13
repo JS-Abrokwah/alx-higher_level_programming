c8b8be00abdca606b8f457dd032ae57464566b77
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-04-13 23:42:39 +0000

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Creates a pascal's triangle"
+"""
+
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    while len(pascal) < n:
+        row_ele = pascal[-1]
+        row = [1]
+
+        for i in range(len(row_ele) - 1):
+            row.append(row_ele[i] + row_ele[i + 1])
+        row.append(1)
+        pascal.append(row)
+
+    return pascal
