ad1d66377af6b4ec00b428e28c5ca72f670ba970
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-05-08 11:56:09 +0000

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,33 @@ class Rectangle(Base):
             y(int): The y coordinate of the new Rectangle instance
             id(int): The new Rectangle instance's id
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be > 0")
+        else:
+            self.__y = y
         super().__init__(id)
 
     """Getters"""
